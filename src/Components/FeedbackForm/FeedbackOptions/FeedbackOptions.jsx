f92cc66f6802607428580bb5c5898a86699d5725
Viacheslav Plevako <cheryec@ukr.net>
2023-03-10 12:17:59 +0200

    add PropTypes

--- src/Components/FeedbackForm/FeedbackOptions/FeedbackOptions.jsx
+++ src/Components/FeedbackForm/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div className={css.btnlist}>
       {options.map(nameEl => (
-        <button onClick={onLeaveFeedback} className={css.button} type="button">
+        <button key={nameEl} onClick={onLeaveFeedback} className={css.button} type="button">
           {nameEl}
         </button>
       ))}
@@ -14,3 +15,8 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 };
 
 export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
--- src/Components/FeedbackForm/Section/Section.jsx
+++ src/Components/FeedbackForm/Section/Section.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import css from './Section.module.css';
 
 const Section = ({ title, children }) => (
@@ -9,3 +10,7 @@ const Section = ({ title, children }) => (
 );
 
 export default Section;
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
